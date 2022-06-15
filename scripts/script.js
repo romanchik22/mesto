d449093dfad2b0d2027ea36829380e4f3a52b8b9
Roman Pinaev <romanchikpinaev@yandex.ru>
2022-06-15 13:27:34 +0300

    создан popup с js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,33 @@
-function buttonClick() {
-    console.log('333333');
+
+let profileEditButton = document.querySelector('.profile__edit');
+let popup = document.querySelector('.popup');
+let profileEditing = document.querySelector('.profile-editing');
+let profileEditingCloseButton = document.querySelector('.profile-editing__close');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let inputName = document.querySelector('.input_name');
+let inputJob = document.querySelector('.input_job');
+let formElement = document.querySelector('.profile-editing__form');
+
+function showPopup() {
+    popup.classList.remove('hidden');
+    profileEditing.classList.remove('hidden');
+    inputName.value = profileName.innerText;
+    inputJob.value = profileJob.innerText; 
 }
+
+function hiddenPopup() {    
+    popup.classList.add('hidden');
+    profileEditing.classList.add('hidden');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    hiddenPopup();
+    profileName.textContent = inputName.value;
+    profileJob.textContent = inputJob.value; 
+}
+
+profileEditButton.addEventListener('click', showPopup);
+profileEditingCloseButton.addEventListener('click', hiddenPopup);
+formElement.addEventListener('submit', formSubmitHandler);
