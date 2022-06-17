91d597f4272b86576e5152545adfa3ffc5c814fb
mihsastv <mihsastv@yandex.ru>
2022-06-17 15:32:22 +0300

    Правки пор чеклисту

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,37 @@
-
 let profileEditButton = document.querySelector('.profile__edit');
 let popup = document.querySelector('.popup');
-let profileEditingCloseButton = document.querySelector('.profile-editing__close');
+let popupClose = document.querySelector('.popup__close');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-let inputName = document.querySelector('.input_name');
-let inputJob = document.querySelector('.input_job');
-let formElement = document.querySelector('.profile-editing__form');
+let inputName = document.querySelector('.popup__input_name');
+let inputJob = document.querySelector('.popup__input_job');
+let formElement = document.querySelector('.popup__form');
 let placeButtonFavorite = document.querySelectorAll('.place__button-favorite');
 
 
 function showPopup() {
-    popup.classList.remove('popup_hidden');
+    popup.classList.add('popup_opened');
     inputName.value = profileName.innerText;
-    inputJob.value = profileJob.innerText; 
+    inputJob.value = profileJob.innerText;
 }
 
-function hiddenPopup() {    
-    popup.classList.add('popup_hidden');
+function hiddenPopup() {
+    popup.classList.remove('popup_opened');
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     hiddenPopup();
     profileName.textContent = inputName.value;
-    profileJob.textContent = inputJob.value; 
+    profileJob.textContent = inputJob.value;
 }
 
 function changeFovorite (event) {
-    event.target.classList.toggle('place__button-favorite_black');   
-} 
+    event.target.classList.toggle('place__button-favorite_black');
+}
 
 profileEditButton.addEventListener('click', showPopup);
-profileEditingCloseButton.addEventListener('click', hiddenPopup);
+popupClose.addEventListener('click', hiddenPopup);
 formElement.addEventListener('submit', formSubmitHandler);
 placeButtonFavorite.forEach(function (el) {
     el.addEventListener('click', changeFovorite);
