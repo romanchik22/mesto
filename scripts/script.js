d2662cb50cbf66be71bc03df60a5a94d76031330
Roman Pinaev <romanchikpinaev@yandex.ru>
2022-06-24 19:07:28 +0300

    создан templete

--- scripts/script.js
+++ scripts/script.js
@@ -35,4 +35,58 @@ popupClose.addEventListener('click', hiddenPopup);
 formElement.addEventListener('submit', formSubmitHandler);
 placeButtonFavorite.forEach(function (el) {
     el.addEventListener('click', changeFavorite);
+}); 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+const placesContainer = document.querySelector(".elements");
+const placeTemplate = document.querySelector("#place-template").content;
+
+const placeInfo = initialCards0(function (card) {
+  return {
+    name: card.name,
+    link: card.link
+  };
 });
+
+function render() {
+  placeInfo.forEach(renderCard);
+}
+
+function renderCard({ name, link }) {
+  const placeElement = placeTemplate
+    .querySelector(".grid-element")
+    .cloneNode(true);
+  placeElement.querySelector(".grid-element__title").textContent = name;
+  placeElement.querySelector(".grid-element__image").src = link;
+
+  placesContainer.prepend(placeElement);
+}
+
+render();
+
+
